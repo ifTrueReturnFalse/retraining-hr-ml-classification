ab679d00ee0885e94dbe23346b129975eae86215
Timothé Duquesne <timothe.duquesne@gmail.com>
2026-01-28 22:29:58 +0100

    fix: changed function output in order to work with preprocessor

--- src/utils.py
+++ src/utils.py
@@ -2,11 +2,11 @@
 import pandas as pd
 from typing import Union
 
-def text_to_numeric(data: Union[pd.Series, pd.DataFrame]) -> pd.Series:
+def text_to_numeric(data: Union[pd.Series, pd.DataFrame]) -> pd.DataFrame:
     if isinstance(data, pd.DataFrame):
         serie = data.iloc[:, 0]
     else:
         serie = data
 
     cleaned = pd.to_numeric(serie.astype(str).str.extract(r'(\d+)')[0])
-    return cleaned.fillna(0)
+    return cleaned.fillna(0).to_frame()
